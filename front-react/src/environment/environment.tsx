3e0d88f210d7da3657bc4e97ffa0dbb12fea7391
Lionel AREL <lionel.arel@outlook.fr>
2022-10-02 19:51:41 +0200

    url rectifiying

--- front-react/src/environment/environment.tsx
+++ front-react/src/environment/environment.tsx
@@ -1,5 +1,5 @@
 const environments = (!process.env.NODE_ENV || process.env.NODE_ENV === 'development') ? 
 {baseUrl:"127.0.0.1:8000",protocol:"http://"} :
-{baseUrl:"www.whatsapp-lionel-arel.ga/backend",protocol:"https://"}
+{baseUrl:"www.whatsapp-clone-lionel-arel.ga/backend",protocol:"https://"}
 
 export default environments;
