f0ad0330aebcaf04679a66ff8e5519497350c4c2
Lionel AREL <lionel.arel@outlook.fr>
2022-11-22 21:23:45 +0100

    update requirements and env files

--- back-django/api/settings.py
+++ back-django/api/settings.py
@@ -1,11 +1,10 @@
-import os
 from pathlib import Path
 import botocore 
-import boto3
 import botocore.session 
 from aws_secretsmanager_caching import SecretCache, SecretCacheConfig 
 import json
-from datetime import timedelta
+import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -15,13 +14,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@un!c(fd$gxcik)*#pqv4lw!=n#=sep7pc1rt+%_riq)ti**xy'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-ALLOWED_HOSTS = ['*']
-
+DEBUG = config('DEBUG',cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS',default='*',cast=lambda v: [s.strip() for s in v.split(',')])
 
 # Application definition
 
@@ -77,40 +74,30 @@ ASGI_APPLICATION = "api.asgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-try:
-    client = botocore.session.get_session().create_client(service_name='secretsmanager',region_name='eu-west-3')
-    cache_config = SecretCacheConfig()
-    cache = SecretCache( config = cache_config, client = client)
+client = botocore.session.get_session().create_client(service_name='secretsmanager',region_name='eu-west-3')
+cache_config = SecretCacheConfig()
+cache = SecretCache( config = cache_config, client = client)
 
-    secret_name = 'djangoEcommerce'
+secret_name = 'amazon/clone'
+
+try :
     secret = json.loads(cache.get_secret_string(secret_name))['password']
+except Exception as e:
+    secret = None
 
-    ssm = boto3.client('ssm')
-    parameter = ssm.get_parameter(Name='db-endpoint', WithDecryption=True)
-    endpoint = parameter['Parameter']['Value']
-    print(endpoint)
+###DATABASE
+#SESSION_ENGINE = config('SESSION_ENGINE')
+DATABASES = {
+    'default': {
+        'ENGINE': config('DB_ENGINE'),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': secret if(secret is not None) else '',
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+    }
+}
 
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'djangoEcommerce',
-            'USER': 'postgres',
-            'PASSWORD': secret,
-            'HOST': endpoint,
-            'PORT': '5432',
-        }
-    }
-except:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'dbWhatsapp',
-            'USER': 'postgres',
-            'PASSWORD': "dbWhatsapp",
-            'HOST': 'dbwhatsapp.coivorc7u40d.eu-west-3.rds.amazonaws.com',
-            'PORT': '5432',
-        }
-    }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -134,7 +121,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-en'
+LANGUAGE_CODE = config('LANGUAGE_CODE')
 
 TIME_ZONE = 'UTC'
 
@@ -143,12 +130,6 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
-
-STATIC_URL = 'static/'
-
-STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -163,86 +144,56 @@ REST_FRAMEWORK = {
 
 AUTH_USER_MODEL = 'core.User'
 
-CSRF_TRUSTED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    "https://www.chat-lionel-arel.ga",
-    "https://chat-lionel-arel.ga"
-]
 
-CORS_ALLOW_CREDENTIALS = True
-CORS_ORIGIN_ALLOW_ALL = True
+###CORS
+CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS',default=[],cast=lambda v: [s.strip() for s in v.split(',')])
+CORS_ALLOWED_ORIGINS = config('CORS_ALLOWED_ORIGINS',default=[],cast=lambda v: [s.strip() for s in v.split(',')])
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    "https://www.chat-lionel-arel.ga",
-    "https://chat-lionel-arel.ga"
-]
-
-CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    "https://www.chat-lionel-arel.ga",
-    "https://chat-lionel-arel.ga"
-]
+CORS_ALLOW_CREDENTIALS = config('CORS_ALLOW_CREDENTIALS',default=True,cast=bool)
 CORS_EXPOSE_HEADERS = ['Content-Type', 'X-CSRFToken']
-CSRF_COOKIE_SAMESITE = 'None'
-SESSION_COOKIE_SAMESITE = 'None'
-
-CSRF_COOKIE_HTTPONLY = False
-SESSION_COOKIE_HTTPONLY = False
-
-CSRF_COOKIE_SECURE = True
-SESSION_COOKIE_SECURE = True
 
 
-SESSION_COOKIE_DOMAIN = '.chat-lionel-arel.ga'
-CSRF_COOKIE_DOMAIN = '.chat-lionel-arel.ga'
+CSRF_COOKIE_SAMESITE = config('CSRF_COOKIE_SAMESITE',default='None')
+SESSION_COOKIE_SAMESITE = config('SESSION_COOKIE_SAMESITE',default='None')
 
-CHANNEL_LAYERS = {
-    'default': {
-        'BACKEND': "channels_redis.core.RedisChannelLayer",
-        'CONFIG': {
-            'hosts': [('127.0.0.1', 6379)],
-        },
+CSRF_COOKIE_HTTPONLY = config('CSRF_COOKIE_HTTPONLY',default=False,cast=bool)
+SESSION_COOKIE_HTTPONLY = config('SESSION_COOKIE_HTTPONLY',default=False,cast=bool)
+
+CSRF_COOKIE_SECURE = config('CSRF_COOKIE_SECURE',default=True,cast=bool)
+SESSION_COOKIE_SECURE = config('SESSION_COOKIE_SECURE',default=True,cast=bool)
+
+CORS_ORIGIN_ALLOW_ALL = config('CORS_ORIGIN_ALLOW_ALL',default=True,cast=bool)
+CSRF_ORIGIN_ALLOW_ALL = config('CORS_ORIGIN_ALLOW_ALL',default=True,cast=bool)
+	
+SESSION_COOKIE_DOMAIN = config('SESSION_COOKIE_DOMAIN',default=None)
+CSRF_COOKIE_DOMAIN = config('CSRF_COOKIE_DOMAIN',default=None)
+
+##STORAGE
+DEFAULT_FILE_STORAGE = config('DEFAULT_FILE_STORAGES')
+STATICFILES_STORAGE = config('STATICFILES_STORAGES')
+
+### STATIC
+STATIC_URL = config('STATIC_URL')
+STATIC_ROOT = BASE_DIR / config('STATIC_ROOT')
+MEDIA_ROOT = BASE_DIR / 'staticmedias/'
+
+
+if config('REDIS_BACK') == 'channels.layers.InMemoryChannelLayer' :
+    CHANNEL_LAYERS = {
+        'default': {
+            'BACKEND': config('REDIS_BACK'),
+            # 'CONFIG': {
+            #     'hosts': [(config('REDIS_IP'), config('REDIS_PORT',cast=int))],
+            # },
+        }
     }
-}
-
-LOGGING = {
-    'version': 1,
-    'formatters': {
-        'verbose': {
-            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
-        },
-        'simple': {
-            'format': '%(levelname)s %(message)s'
-        },
-    },
-    'handlers': {
-        'console': {
-            'level': 'DEBUG',
-            'class': 'logging.StreamHandler',
-            'formatter': 'simple'
-        },
-        'file': {
-            'level': 'DEBUG',
-            'class': 'logging.FileHandler',
-            'filename': '/var/log/django/django-error.log',
-            'formatter': 'simple'
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
-        'daphne': {
-            'handlers': ['file'],
-            'level': 'DEBUG',
-            'propagate': True,
-        },
+else :
+    CHANNEL_LAYERS = {
+        'default': {
+            'BACKEND': config('REDIS_BACK'),
+            'CONFIG': {
+                'hosts': [(config('REDIS_IP'), config('REDIS_PORT',cast=int))],
+            },
+        }
     }
-}
 
